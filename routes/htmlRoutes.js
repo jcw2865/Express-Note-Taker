5aac031e9414a35986446f8fcf85b8b03172e1c4
Jake Walker <jcw2865@gmail.com>
2020-01-25 10:26:05 -0600

    Added routes to htmlRoutes.js.

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -7,17 +7,18 @@ const router = require("express").Router();
 // ROUTES
 // ==========
 
-module.exports = function (app) {
-    // HTML GET Requests
-    // Below code handles when users "visit" a page.
-    // In each of the below cases the user is shown an HTML page of content
-    // ---------------------------------------------------------------------------
+// HTML GET Requests
+// Below code handles when users "visit" a page.
+// In each of the below cases the user is shown an HTML page of content
+// ---------------------------------------------------------------------------
 
-    app.get("/notes", function (req, res) {
-        res.sendFile(path.join(__dirname, "../public/notes.html"));
-    });
+app.get("/notes", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/notes.html"));
+});
 
-    app.get("*", function (req, res) {
-        res.sendFile(path.join(__dirname, "../public/index.html"));
-    });
-};
+// If no matching route is found default to home
+app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+});
+
+module.exports = router;
